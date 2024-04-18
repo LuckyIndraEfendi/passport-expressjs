0366df66fee8fd2a1e9d5dfdeea52a8aed5239c8
LuckyIndraEfendi <kimilbonchu@gmail.com>
2024-04-18 19:52:10 +0700

    update into boostrap

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ connectDB();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "dist")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
